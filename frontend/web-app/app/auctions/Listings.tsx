88812a710faf770f26e8bb3d43732fde2d5f8a07
JohnyCoding <vanja.kocubovski@gmail.com>
2024-11-03 21:19:05 +0100

    Notification functionality added to client app

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -3,15 +3,15 @@ import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
 import AppPagination from "../components/AppPagination";
 import { getData } from "../actions/auctionActions";
-import { Auction, PagedResult } from "@/types";
 import Filters from "./Filters";
 import { useParamsStore } from "@/hooks/useParamsStore";
 import { useShallow } from "zustand/shallow";
 import qs from "query-string";
 import EmptyFilter from "../components/EmptyFilter";
+import { useAuctionStore } from "@/hooks/useAuctionStore";
 
 export default function Listings() {
-	const [data, setData] = useState<PagedResult<Auction>>();
+	const [loading, setLoading] = useState(true);
 	const params = useParamsStore(
 		useShallow((state) => ({
 			pageNumber: state.pageNumber,
@@ -23,6 +23,14 @@ export default function Listings() {
 			winner: state.winner,
 		}))
 	);
+	const data = useAuctionStore(
+		useShallow((state) => ({
+			auctions: state.auctions,
+			totalCount: state.totalCount,
+			pageCount: state.pageCount,
+		}))
+	);
+	const setData = useAuctionStore((state) => state.setData);
 	const setParams = useParamsStore((state) => state.setParams);
 	const url = qs.stringifyUrl({ url: "", query: params });
 
@@ -33,10 +41,11 @@ export default function Listings() {
 	useEffect(() => {
 		getData(url).then((data) => {
 			setData(data);
+			setLoading(false);
 		});
-	}, [url]);
+	}, [url, setData]);
 
-	if (!data) return <h3>Loading...</h3>;
+	if (loading) return <h3>Loading...</h3>;
 
 	return (
 		<>
@@ -46,7 +55,7 @@ export default function Listings() {
 			) : (
 				<>
 					<div className="grid grid-cols-4 gap-6">
-						{data.results?.map((auction) => (
+						{data.auctions.map((auction) => (
 							<AuctionCard key={auction.id} auction={auction} />
 						))}
 					</div>
